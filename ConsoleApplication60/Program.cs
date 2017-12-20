15c7c92898fa721e8a894c5042a17c172bcf33d0
Piotrek <@>
2017-12-20 14:03:53 +0100

    przeslanie plików

---
+++ ConsoleApplication60/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApplication60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            refrenSolo();
+            coupleSolo();
+            refrenSolo();
+        }
+        //Jingle Bells
+        static void refrenSolo()
+        {
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Thread.Sleep(300);
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Thread.Sleep(300);
+            Console.Beep(659, 300);
+            Console.Beep(783, 300);
+            Console.Beep(523, 300);
+            Console.Beep(587, 300);
+            Console.Beep(659, 300);
+            Console.Beep(261, 300);
+            Console.Beep(293, 300);
+            Console.Beep(329, 300);
+            Console.Beep(698, 300);
+            Console.Beep(698, 300);
+            Console.Beep(698, 300);
+            Thread.Sleep(300);
+            Console.Beep(698, 300);
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Thread.Sleep(300);
+            Console.Beep(659, 300);
+            Console.Beep(587, 300);
+            Console.Beep(587, 300);
+            Console.Beep(659, 300);
+            Console.Beep(587, 300);
+            Thread.Sleep(300);
+            Console.Beep(783, 300);
+            Thread.Sleep(300);
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Thread.Sleep(300);
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Thread.Sleep(300);
+            Console.Beep(659, 300);
+            Console.Beep(783, 300);
+            Console.Beep(523, 300);
+            Console.Beep(587, 300);
+            Console.Beep(659, 300);
+            Console.Beep(261, 300);
+            Console.Beep(293, 300);
+            Console.Beep(329, 300);
+            Console.Beep(698, 300);
+            Console.Beep(698, 300);
+            Console.Beep(698, 300);
+            Thread.Sleep(300);
+            Console.Beep(698, 300);
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Thread.Sleep(300);
+            Console.Beep(783, 300);
+            Console.Beep(783, 300);
+            Console.Beep(698, 300);
+            Console.Beep(587, 300);
+            Console.Beep(523, 600);
+            Thread.Sleep(600);
+        }
+        static void coupleSolo()
+        {
+            Console.Beep(392, 300);
+            Console.Beep(659, 300);
+            Console.Beep(587, 300);
+            Console.Beep(523, 300);
+            Console.Beep(392, 600);
+            Thread.Sleep(300 * 2);
+            Console.Beep(392, 300);
+            Console.Beep(659, 300);
+            Console.Beep(587, 300);
+            Console.Beep(523, 300);
+            Console.Beep(440, 600);
+            Thread.Sleep(600);
+            Console.Beep(440, 300);
+            Console.Beep(698, 300);
+            Console.Beep(659, 300);
+            Console.Beep(587, 300);
+            Console.Beep(783, 600);
+            Thread.Sleep(600);
+            Console.Beep(880, 300);
+            Console.Beep(880, 300);
+            Console.Beep(783, 300);
+            Console.Beep(622, 300);
+            Console.Beep(659, 600);
+            Thread.Sleep(600);
+            Console.Beep(392, 300);
+            Console.Beep(659, 300);
+            Console.Beep(587, 300);
+            Console.Beep(523, 300);
+            Console.Beep(392, 600);
+            Thread.Sleep(600);
+            Console.Beep(392, 300);
+            Console.Beep(659, 300);
+            Console.Beep(587, 300);
+            Console.Beep(523, 300);
+            Console.Beep(440, 600);
+            Thread.Sleep(600);
+            Console.Beep(440, 300);
+            Console.Beep(698, 300);
+            Console.Beep(659, 300);
+            Console.Beep(587, 300);
+            Console.Beep(783, 600);
+            Thread.Sleep(600);
+            Console.Beep(880, 300);
+            Console.Beep(783, 300);
+            Console.Beep(698, 300);
+            Console.Beep(587, 300);
+            Console.Beep(523, 600);
+            Thread.Sleep(600);
+        }
+    }
+}
